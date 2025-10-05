be40f37789277c92559612f58a5ada4c97a853cc
KashishAgrahari23 <kashishagrahari23@example.com>
2025-10-05 22:45:35 +0530

    weather app

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -3,13 +3,17 @@ import React, { useState } from 'react'
 
 const SearchBar = () => {
     const[city , setCity] = useState("")
+    const handleOnChange=(e)=>{
+        setCity(e.target.value)
+        console.log(e.target.value)
+    }
     const handleClick=()=>{
-        
+        console.log(city)
     }
     
   return (
     <div>
-        <input type="text"  placeholder='search a city name' onChange={(e)=>setCity(e.target.value)} />
+        <input type="text"  placeholder='search a city name' onChange={handleOnChange} />
         <button onClick={handleClick} >Submit</button>
     </div>
   )
