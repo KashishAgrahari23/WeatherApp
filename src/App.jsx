a3d01a4236f07f6af6f7ddeb4761c0b33b9ced3e
KashishAgrahari23 <kashishagrahari23@gmail.com>
2025-10-16 23:50:15 +0530

    weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 import SearchBar from './component/SearchBar'
+import useFetch from './hooks/useFetch'
 
 function App() {
-  
+  const [city , setCity] = useState(null)
+  API_KEy = process.env.API_KEy
+  API_URL = city ? `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEy}&units=metric` : " "
+
+  const {data , loading , error} = useFetch(API_URL)
+
+  const handleSearch(searchCity) => {
+    setCity(searchCity)
+  }
+
+  console.log({data, loading,error})
 
   return (
     <>
